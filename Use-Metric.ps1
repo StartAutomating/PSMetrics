3616bc8808807a50dd1f75cd79f8be63040f780a
StartAutomating <StartAutomating@github.com>
2023-04-29 23:38:54 +0000

    Adding Use-Metric (Fixes #5)

---
+++ Use-Metric.ps1
@@ -0,0 +1,94 @@
+function Use-Metric {
+    [CmdletBinding(PositionalBinding=$false)]
+    param(
+    [Parameter(ValueFromPipeline)]
+    $InputObject,
+    [Parameter(Position=0)]
+    [string]
+    $OutputPath,
+    [Parameter(ValueFromRemainingArguments)]
+    $Arguments
+    )
+    dynamicParam {
+        $metricMatcher = "(?<MetricName>\S+)(?<Suffix>(?>∑|📈|📉|📊|◕|◔|Chart|Metric|PSMetric))"
+        if ($MyInvocation.InvocationName -eq $MyInvocation.MyCommand.Name) { return }
+        if ($myInvocation.InvocationName -notmatch $metricMatcher) { return }
+        $metricMatch = @{} + $matches
+        $MetricCommand = Get-Metric $metricMatch.MetricName
+        if (-not $MetricCommand) { return }
+        $Intention = 
+            if ($metricMatch.Suffix -eq "∑") {
+                'Metric'
+            } elseif ($metricMatch.Suffix -eq '📈') {
+                'LineAscending'
+            } elseif ($metricMatch.Suffix -eq '📉') {
+                'LineDescending'
+            } elseif ($metricMatch.Suffix -eq "📊") {
+                'BarChart'
+            } 
+            elseif ($metricMatch.Suffix -eq '◕') {
+                'PieAscending'
+            }
+            elseif ($metricMatch.Suffix -eq '◔') {
+                'PieDescending'
+            }
+            elseif ($metricMatch.Suffix -like '*Chart*') {
+                'Chart'
+            } elseif ($metricMatch.Suffix -like '*Metric*') {
+                'Metric'
+            }
+    }
+    begin {
+        $accumulateInput = [Collections.Queue]::new()
+    }
+    process {
+        if ($InputObject) {
+            $accumulateInput.Enqueue($InputObject)
+        }        
+    }
+    end {
+        if (-not $MetricCommand) { return }
+        $metricOutput = @($accumulateInput.ToArray() | & $MetricCommand)
+        $commandOutput = 
+            if ($Intention -eq 'Metric') {
+                $metricOutput
+            } else {
+                if ($Intention -like '*Descending*') {
+                    [Array]::Reverse($metricOutput)
+                }
+                
+                [PSCustomObject][Ordered]@{
+                    PSTypeName     = 'Chart'
+                    ChartData      = $metricOutput
+                    ChartType      = if ($Intention -match 'Line') {
+                        'line'
+                    } elseif ($Intention -match 'Bar') {
+                        'bar'
+                    } elseif ($Intention -match 'Pie') {
+                        'pie'
+                    } else {
+                        ''
+                    }
+                    MetricCommand  = $MetricCommand
+                    MetricName     = $MetricCommand.Name -replace '\.metric\.ps1$'
+                } |
+                Out-String -Width 1mb
+            }
+        if ($OutputPath) {            
+            $exporter = $ExecutionContext.SessionState.InvokeCommand.GetCommand(
+                "Export-$(@($OutputPath -split '\.')[-1])",
+                'Function,Alias,Cmdlet'
+            )
+            if ($exporter) {
+                $metricOutput | & $exporter $OutputPath
+            } else {
+                $metricOutput | Set-Content $OutputPath
+            }
+            if (Test-Path $OutputPath) {
+                Get-Item $OutputPath
+            }
+            
+        }
+    }
+}
+
