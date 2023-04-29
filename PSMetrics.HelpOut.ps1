3c04453d0e8c9c9594a0379c012a3543c42a16b1
James Brundage <@github.com>
2023-04-29 16:49:22 -0700

    Adding PSMetrics.HelpOut.ps1 (re #2)

---
+++ PSMetrics.HelpOut.ps1
@@ -0,0 +1,8 @@
+#requires -Module HelpOut
+Push-Location $PSScriptRoot
+
+Import-Module .\PSMetrics.psd1
+
+Save-MarkdownHelp -Module PSMetrics -ScriptPath mETRICS -ReplaceScriptName '\.metric\.ps1$' -ReplaceScriptNameWith "-Metric" -PassThru
+
+Pop-Location
