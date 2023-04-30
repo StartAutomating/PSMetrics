db1d648dcc1e293119bb52d5f571eca27138fcb5
James Brundage <@github.com>
2023-04-29 17:04:38 -0700

    PSMetrics Helpout - refining pattern (using negative lookbehind) (re #2)

--- PSMetrics.HelpOut.ps1
+++ PSMetrics.HelpOut.ps1
@@ -3,6 +3,6 @@ Push-Location $PSScriptRoot
 
 Import-Module .\PSMetrics.psd1
 
-Save-MarkdownHelp -Module PSMetrics -ScriptPath '(?<!PS)Metrics[\\/]' -ReplaceScriptName '\.metric\.ps1$' -ReplaceScriptNameWith "-Metric" -PassThru
+Save-MarkdownHelp -Module PSMetrics -ScriptPath '(?<!(?>PS|-))Metrics[\\/]' -ReplaceScriptName '\.metric\.ps1$' -ReplaceScriptNameWith "-Metric" -PassThru
 
 Pop-Location
