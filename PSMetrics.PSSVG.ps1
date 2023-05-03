bd7f826c95e356d54b52c2833fbcc56747c9b6f1
James Brundage <@github.com>
2023-05-03 13:44:51 -0700

    Adding PNG version of logo (re #13)

--- PSMetrics.PSSVG.ps1
+++ PSMetrics.PSSVG.ps1
@@ -9,16 +9,12 @@ if (-not (Test-Path $assetsPath)) {
 }
 
 $fontName = 'Montserrat'
-svg -ViewBox 1920,1080 @(
-    $psChevron.svg.symbol
-
+svg -ViewBox 1920,1080 @(    
     svg.defs @(
         SVG.GoogleFont -FontName $fontName
     )
-
+    $psChevron.svg.symbol
  
     SVG.text -Content "PSMetrics" -Style "font-family: '$FontName', sans-serif" -X 50% -Y 50% -TextAnchor 'middle' -AlignmentBaseline 'middle' -DominantBaseline 'middle' -FontSize 15em -Fill '#4488ff' -Class 'foreground-fill'
     SVG.use -Href '#psChevron' -Fill '#4488ff' -Height 7.5% -X -26.5% -Y 42%
-        
-    # SVG.ArcPath -Start $circleTop -End $circleRight -Radius $radius -Fill '#4488ff' -Sweep
 ) -OutputPath (Join-Path $assetsPath "PSMetrics.svg")
