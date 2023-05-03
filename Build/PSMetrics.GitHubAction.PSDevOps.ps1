be22d0dc47f3955fb7fc6f922f373681a5363e11
James Brundage <@github.com>
2023-05-03 14:17:17 -0700

    Adding PSMetrics Action Source (re #14)

---
+++ Build/PSMetrics.GitHubAction.PSDevOps.ps1
@@ -0,0 +1,8 @@
+﻿#requires -Module PSDevOps
+#requires -Module PSMetrics
+Import-BuildStep -ModuleName PSMetrics
+Push-Location ($PSScriptRoot | Split-Path)
+New-GitHubAction -Name "Metrics" -Description @'
+Measure Metrics and Create Charts with PSMetrics
+'@ -Action PSMetricsAction -Icon pie-chart -OutputPath .\action.yml
+Pop-Location
